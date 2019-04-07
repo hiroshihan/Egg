8c18a457fc6ee0498603a39166ed2af02c3fc19c
student <student@localhost>
2019-04-07 20:26:52 +0900

    Servlet結合確認

--- src/servlet/CommonServlet.java
+++ src/servlet/CommonServlet.java
@@ -1,6 +1,9 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,20 +17,40 @@ import javax.servlet.http.HttpServletResponse;
 public class CommonServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	public CommonServlet(HttpServletRequest request) throws UnsupportedEncodingException {
+		request.setCharacterEncoding("UTF-8");
+	}
+
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
+	public void connectJsp(HttpServletRequest request, HttpServletResponse response, String path, String jspName,
+			RequestDispatcher rd) throws ServletException, IOException {
+
+		StringBuilder stringBuilder = new StringBuilder("/");
+		if (path != null && !path.equals("")) {
+			stringBuilder.append(path).append("/");
+		}
+		String jspLink = stringBuilder.append(jspName).append(".jsp").toString();
+
+		rd = request.getRequestDispatcher(jspLink);
+		rd.forward(request, response);
+	}
 }
--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -1,8 +1,7 @@
 package servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.logging.LoggingMXBean;
+import java.io.UnsupportedEncodingException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,15 +9,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
 
 
 /**
  * Servlet implementation class LoginServlet
  */
 @WebServlet("/LoginServlet")
-public class LoginServlet extends CommonServlet {
-	
+//public class LoginServlet extends CommonServlet {
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;	
+
+	public LoginServlet(HttpServletRequest request) throws UnsupportedEncodingException {
+		//super(request);
+		super();
+		request.setCharacterEncoding("UTF-8");
+	}
+
 	private String USER = "-";
 	private String PASS = "-";
 	/**
@@ -26,9 +32,6 @@ public class LoginServlet extends CommonServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTf-8");
-		
-		
-		PrintWriter out = response.getWriter();
 		
 		String action = request.getParameter("action");
 		if(action.equals("login")) {
