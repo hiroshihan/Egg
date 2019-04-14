fb67abc75f2e115dbaefa67084bf54bbdaaed7ac
student <student@localhost>
2019-04-14 17:06:49 +0900

    WebContent内を更新しました。

---
+++ src/servlet/InsertUserServlet.java
@@ -0,0 +1,39 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InsertUserServlet
+ */
+@WebServlet("/InsertUserServlet")
+public class InsertUserServlet extends CommonServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see CommonServlet#CommonServlet()
+     */
+    public InsertUserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		setCharacterEncoding(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
