819e566b0fabd3e6ba0b3d4bf1eaf20ed06688cf
student <student@localhost>
2019-05-12 20:34:43 +0900

    直接masterにコミット（諸々修正）

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -7,49 +7,43 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import dao.UserDao;
-import dao.error.DAOException;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends CommonServlet {
 	private static final long serialVersionUID = 1L;
-	private String USER = "-";
-	private String PASS = "-";
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		super.setCharacterEncoding(request, response);
 		String action = request.getParameter("action");
 
-		if (action.equals("login")) {
-			String name = request.getParameter("username");
-			String pw = request.getParameter("userpassword");
-
-			UserDao dao;
-			try {
-				dao = new UserDao();
-
-				String dbUser = dao.getUserPwd(name);
-				if (USER.equals(name) && PASS.equals(pw)) {
-					connectJsp(request, response, null, "top");
+		try{
+			if (action.equals("login")) {
+				String name = request.getParameter("username");
+				String pw = request.getParameter("userpassword");
+			
+				HttpSession session = request.getSession();
+				UserDao dao = new UserDao();
+				String dbpw = dao.getUserPwd(name);
+				if (dbpw.equals(pw)) {
+					session.setAttribute("isLogin", "true");
+					connectJsp(request, response, null, "index");
 				} else {
-					HttpSession session = request.getSession();
-					session.setAttribute("isLogin", "false");
 					connectJsp(request, response, null, "login");
 				}
+			} else if (action.equals("logout")) {
+				HttpSession session = request.getSession(false);
+				if (session != null) {
+					session.invalidate();
+				}
+				connectJsp(request, response, null, "index");
+			} else if (action.equals("login_view")) {
 				connectJsp(request, response, null, "login");
-			} catch (DAOException e) {
-				// TODO 自動生成された catch ブロック
-				e.printStackTrace();
 			}
-		} else if (action.equals("logout")) {
-			HttpSession session = request.getSession(false);
-			if (session != null) {
-				session.invalidate();
-			}
+		} catch(Exception e){
+			e.printStackTrace();
 		}
-
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
